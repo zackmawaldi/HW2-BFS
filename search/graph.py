4e3158caa49278a370b41f8d765d6ee55b7b43ba
zack <yazan.mawaldi@gmail.com>
2024-01-23 15:28:49 -0800

    implement `bfs` is `Graph`

--- search/graph.py
+++ search/graph.py
@@ -1,3 +1,4 @@
+import queue
 import networkx as nx
 
 class Graph:
@@ -14,15 +15,51 @@ class Graph:
 
     def bfs(self, start, end=None):
         """
-        TODO: write a method that performs a breadth first traversal and pathfinding on graph G
+        Performs a breadth first traversal and pathfinding on graph G
+
+        Args:
+        start: start node
+        end (optional): end node
 
         * If there's no end node input, return a list nodes with the order of BFS traversal
         * If there is an end node input and a path exists, return a list of nodes with the order of the shortest path
         * If there is an end node input and a path does not exist, return None
 
         """
-        return
+        graph = self.graph
 
+        if type(graph) != type(nx.DiGraph()):
+            raise TypeError(f"You're graph is of type {type(graph)} not {type(nx.Graph())}")
 
+        q = queue.Queue()
+        visited = set()
 
+        distances = {start: (0,[start], None)}
+                    # node: (distance, path, parent)
+        
+        q.put(start)
+        visited.add(start)
+        while q.qsize() != 0:
+            vertix = q.get()
+            neighbors = list(graph.neighbors(vertix)) # list casting is to avoid a wierd bug...
 
+            for n in neighbors:
+                if n not in visited:
+                    visited.add(n)
+                    q.put(n)
+
+                    # retrive runtime by getting parent path + current node n
+                    new_path = distances[vertix][1] + [n]
+                    dis = len(new_path) - 1
+                    distances[n] = (dis, new_path, vertix)
+                    
+                    # If we found the end goal, return it
+                    if end and n == end:
+                        return distances[n][1]
+
+        # if we finish bfs and didn't hit earlier if return, return None (end not found by bfs)
+        # else, just return bfs visit path (as a list, per requirment)
+        if end:
+            return None
+        else:
+            return list(visited)
